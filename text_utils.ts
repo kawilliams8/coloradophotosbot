0be189506cd68f18d123b47a999d052b723802b5
Katie Williams <kawilliams8@gmail.com>
2024-11-14 15:54:23 -0700

    Add html string decoding

--- text_utils.ts
+++ text_utils.ts
@@ -1,4 +1,5 @@
 import { ScrapedData } from "./types";
+import { decode } from "html-entities";
 
 export function truncate(text: string, maxChars: number) {
   return text.length > maxChars ? text.slice(0, maxChars) + "... " : text;
@@ -15,10 +16,10 @@ export function composePostText({
   // "Last bivouac at Camp Hale... | 1940-1945 | 10th Mountain Division soldiers rest..."
   const dateSeparator = ` | ${imageDate} | `; // image date is a varied string, might be long
   const text =
-    truncate(title, 50) +
+    truncate(decode(title), 50) +
     dateSeparator +
-    (truncate(summary, 245 - dateSeparator.length) ??
-      truncate(altSummary, 245 - dateSeparator.length));
+    (truncate(decode(summary), 245 - dateSeparator.length) ??
+      truncate(decode(altSummary), 245 - dateSeparator.length));
   console.log("Composed post text: ", text);
   return text;
 }
