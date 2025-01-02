027c9c4dbe6225e0feed8f5d1a3e9a30b3cbdc64
Katie Williams <kawilliams8@gmail.com>
2025-01-01 20:40:30 -0700

    On push, go back to original tags syntax

--- index.ts
+++ index.ts
@@ -72,11 +72,12 @@ async function postToBluesky(resizedPath: PathLike, scrapedData: ScrapedData) {
       if (scrapedData.nodeUrl.length) {
         const rt = new RichText({
           text:
-            "#Colorado " +
-            tags[0] + " " +
-            tags[1] + " " +
             "DPL Archive post: " +
-            scrapedData.nodeUrl
+            scrapedData.nodeUrl +
+            " #Colorado " +
+            tags[0] +
+            " " +
+            tags[1],
         });
         await rt.detectFacets(agent); // automatically detects mentions and links
         await agent.post({
